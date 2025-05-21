6b8d9d8252fcb878ed76de2c8b8f9c41ba7c4b21
Erik0631 <erik12jensen@hotmail.com>
2025-05-21 13:18:23 +0200

    Update InputInfo.jsx

--- src/Components/InputInfo.jsx
+++ src/Components/InputInfo.jsx
@@ -4,38 +4,55 @@ export default function Info () {
 
     const Namelist = ["Erik", "Nathalie"] //array med godkjent navn
     const [userName, setUsername] = useState("");
+    const [number,setNumber] = useState("")
     const showForm = !Namelist.includes(userName); // endrer denne til false for å kjule skjemat
 
         const handleInputChange = (event) => {
             setUsername(event.target.value); 
+
         };
 
-
-        if (userName === "Erik") {
-            console.log("Vellkommen")
-        } else {
-            console.log("Taper")
+        const handleNumber = (event) => {
+            setNumber(event.target.value)
         }
+       
+
 
         return (
             <>
             {userName === "Erik" ? (
                 <p>Vellkommen Erik</p>
             ) : (
-                <p>Taper</p>
+                null
+            ) }
+
+            {number === "95252939" ? (
+                <p>Velkommen</p>
+            ) : (
+                null
             ) }
             
-          {showForm && (
-            <form action="/submit">
+      
+            <form action="/Info">
+
                 <input type="text"
                  name="username" 
                  placeholder="Skriv inn navnet ditt, sjef" 
                  value ={userName}
                  onChange= {handleInputChange}
                  />
+                 <input 
+                  type="text"
+                  name="Number"
+                  placeholder="Skriv ditt nummer"
+                  value={number}
+                  onChange={handleNumber}
+                  />
+
+
                 <button type="submit">Submit</button>
             </form>
-            )}
+           
 
             </>
             
